f8c5befc061e94f6cc7717bffb7842b4512252da
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-02-26 12:13:13 +0700

    feat: Setup pwa

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite'
 import eslintPlugin from 'vite-plugin-eslint'
 import svgr from 'vite-plugin-svgr'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import { resolve } from 'path'
 
@@ -37,7 +38,36 @@ export default defineConfig(({ mode }) => {
 	const appEnv = loadEnv(mode, process.cwd(), envPrefix)
 
 	return {
-		plugins: [react(), eslintPlugin(), svgr()],
+		plugins: [
+			react(),
+			eslintPlugin(),
+			svgr(),
+			VitePWA({
+				registerType: 'autoUpdate',
+				injectRegister: 'inline',
+				workbox: {
+					globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+				},
+				manifest: {
+					name: 'React JS Boilerplate',
+					short_name: 'React JS Boilerplate',
+					description: 'My Awesome App description',
+					theme_color: '#ffffff',
+					icons: [
+						{
+							src: 'pwa-192x192.png',
+							sizes: '192x192',
+							type: 'image/png',
+						},
+						{
+							src: 'pwa-512x512.png',
+							sizes: '512x512',
+							type: 'image/png',
+						},
+					],
+				},
+			}),
+		],
 		resolve: {
 			alias: resolveAlias,
 		},
